f661876070b5d352e9c04786192951bfdbee4fba
Patryk Sienniak <patsenn2000@gmail.com>
2022-12-11 21:37:24 +0000

    Demo Commit

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from . import util
+
+from markdown2 import Markdown
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def title(request,title):
